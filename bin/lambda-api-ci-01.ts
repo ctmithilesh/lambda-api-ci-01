071dd57805eb4279ff5bbafd3168c661c6755110
morbiustech <104360312+ctmithilesh@users.noreply.github.com>
2022-08-02 11:42:20 +0530

    added aws-sdk package and resolved bugs

--- bin/lambda-api-ci-01.ts
+++ bin/lambda-api-ci-01.ts
@@ -4,5 +4,5 @@ import { CIStack } from '../lib/ci-stack'
 
 const app = new cdk.App();
 new CIStack(app, 'LambdaApiCi01Stack',{
-    repositoryName:'lambda-api'
+    repositoryName:'lambda-api-ci'
 });
